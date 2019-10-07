39451042e121fa07eacf9d1db0a888496695ed80
Dmitrij Belousov <dmitrij.belousov@gmail.com>
2019-10-07 20:52:46 +0200

    detect env from boot source

--- arch/arm/boards/myir-mys6ul/board.c
+++ arch/arm/boards/myir-mys6ul/board.c
@@ -21,6 +21,8 @@
 #include <init.h>
 #include <mach/bbu.h>
 #include <linux/phy.h>
+#include <environment.h>
+#include <bootsource.h>
 
 #include <linux/micrel_phy.h>
 
@@ -34,14 +36,49 @@ static int ksz8081_phy_fixup(struct phy_device *dev)
 
 static int myir_mys6ul_init(void)
 {
+	int ret;
+	char *environment_path, *default_environment_path;
+	char *envdev, *default_envdev;
+
 	if (!of_machine_is_compatible("myir,imx6ul-mys6ul"))
 		return 0;
 
 	phy_register_fixup_for_uid(PHY_ID_KSZ8081, MICREL_PHY_ID_MASK,
 			ksz8081_phy_fixup);
 
-	imx6_bbu_internal_mmc_register_handler("mmc", "/dev/mmc1.barebox",
-			BBU_HANDLER_FLAG_DEFAULT);
+	default_environment_path = "/chosen/environment-nand";
+	default_envdev = "NAND flash";
+
+	switch (bootsource_get()) {
+	case BOOTSOURCE_MMC:
+		environment_path = basprintf("/chosen/environment-sd%d",
+					       bootsource_get_instance() + 1);
+		envdev = "MMC";
+		break;
+	case BOOTSOURCE_NAND:
+		environment_path = basprintf("/chosen/environment-nand");
+		envdev = "NAND flash";
+		break;
+	case BOOTSOURCE_SPI_NOR:
+		environment_path = basprintf("/chosen/environment-spinor");
+		envdev = "SPI NOR flash";
+		break;
+	default:
+		environment_path = basprintf(default_environment_path);
+		envdev = default_envdev;
+		break;
+	}
+
+	printf("Using environment in %s\n", envdev);
+	if (environment_path) {
+		ret = of_device_enable_path(environment_path);
+		if (ret < 0)
+			pr_warn("Failed to enable environment partition '%s' (%d)\n",
+				environment_path, ret);
+		free(environment_path);
+	}
+
+    imx6_bbu_nand_register_handler("nand", BBU_HANDLER_FLAG_DEFAULT);
 
 	barebox_set_hostname("mys6ul");
 
